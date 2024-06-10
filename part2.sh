30a9cd95edf71d4eb188cf97cdb51c1ec0c9b6b9
syahorau <syahorau@gmail.com>
2024-06-10 14:06:51 +0300

    Finished

--- part2.sh
+++ part2.sh
@@ -5,9 +5,8 @@ wget https://download.virtualbox.org/virtualbox/7.0.18/VBoxGuestAdditions_7.0.18
 sh /mnt/VBoxLinuxAdditions.run 
 sleep 10s
 umount /mnt
+sleep 10s
 rm -rf ./VBoxGuestAdditions_7.0.18.iso 
-umount /virtbox && \
-rm -rf /virtbox && \
 rm -rf /etc/rc.local
 rm -ff /root/part2.sh
 echo '#!/bin/bash
