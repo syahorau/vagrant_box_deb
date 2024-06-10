2bf0e58829929a42f3d8579e23a2b993f8af18cc
syahorau <syahorau@gmail.com>
2024-06-10 13:53:10 +0300

    Finished

--- part2.sh
+++ part2.sh
@@ -7,6 +7,7 @@ conf_folder='//192.168.100.100/docs/itm/'
 mount -t cifs "$conf_folder" /mnt -o guest && \
 mount /mnt/confs/VBoxGuestAdditions.iso /virtbox && \
 sh /virtbox/VBoxLinuxAdditions.run && \
+sleep 10s
 umount /virtbox && \
 rm -rf /virtbox && \
 rm -rf /etc/rc.local
