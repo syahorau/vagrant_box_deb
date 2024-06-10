71fc0b21cf5d91443cce66432de484d6a89c9f3f
syahorau <syahorau@gmail.com>
2024-06-10 13:32:41 +0300

    Finished

--- part2.sh
+++ part2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #create folder
-mkdir /virtbox 
-wget https://download.virtualbox.org/virtualbox/7.0.18/VBoxGuestAdditions_7.0.18.iso && mount ./VBoxGuestAdditions_7.0.18.iso /virtbox 
-sh /virtbox/VBoxLinuxAdditions.run && umount /virtbox && rm -rf /virtbox
-rm -rf ./VBoxGuestAdditions_7.0.18.iso 
+mkdir /virtbox
+#Vars
+conf_folder='//192.168.100.100/docs/itm/'
+#mount conf_folder
+mount -t cifs "$conf_folder" /mnt -o guest && \
+mount /mnt/confs/VBoxGuestAdditions.iso /virtbox && \
+sh /virtbox/VBoxLinuxAdditions.run && \
+umount /virtbox && \
+rm -rf /virtbox && \
 rm -rf /etc/rc.local
 rm -ff /root/part2.sh
 sudo dd if=/dev/zero of=/EMPTY bs=1M
