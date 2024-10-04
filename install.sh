a81d34994331c856d6986d22e944378608c3ea7c
syahorau <syahorau@gmail.com>
2024-10-04 10:18:36 +0300

    Added debian

--- install.sh
+++ install.sh
@@ -6,10 +6,15 @@ a=$(. /etc/os-release && echo "$VERSION_CODENAME")
 if [ "$a" = 'bullseye' ]; then
 echo "deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
-deb http://deb.debian.org/debian-security bullseye/updates main contrib non-free
-deb-src http://deb.debian.org/debian-security bullseye/updates main contrib non-free
+
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" > /etc/apt/sources.list
+deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
+
+deb http://security.debian.org/debian-security/ bullseye-security main contrib non-free
+deb-src http://security.debian.org/debian-security/ bullseye-security main contrib non-free" > /etc/apt/sources.list
 fi
 
 apt update && \
